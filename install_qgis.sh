e98abb5e7e7f89a0394b0bc6534a0441398b8cf7
Carlos López Quintanilla <carlos.lopez@psig.es>
2020-06-24 09:35:07 +0200

    Update install_qgis.sh

--- install_qgis.sh
+++ install_qgis.sh
@@ -15,18 +15,18 @@ deb     http://qgis.org/debian-nightly-release codename main
 deb-src http://qgis.org/debian-nightly-release codename main
 
 ## OPTION B - QGIS Release with ubuntugis dependencies for Ubuntu
-deb	      http://qgis.org/ubuntugis codename main
-deb-src   http://qgis.org/ubuntugis codename main
+deb	    http://qgis.org/ubuntugis codename main
+deb-src http://qgis.org/ubuntugis codename main
 
 ## Update packages
 sudo apt-get update
 
 ## Uninstall QGIS - only if you have old install
-sudo apt-get remove qgis python-qgis
+sudo apt-get remove qgis python-qgis saga grass qgis-plugin-grass grass-doc
 sudo apt-get autoremove
 
 ## Update packages
 sudo apt-get update
 
 ## Install QGIS full (saga, grass, python-qgis, etc.)
-sudo apt-get install -y qgis saga grass python-qgis qgis-plugin-grass grass-doc
+sudo apt-get install -y qgis python-qgis saga grass qgis-plugin-grass grass-doc
